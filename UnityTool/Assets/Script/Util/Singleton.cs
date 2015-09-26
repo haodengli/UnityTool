6b29d8e6f91a9df641298aa466f95edfb28fac5a
haodengli <1251280100@qq.com>
2015-09-26 12:05:47 +0800

    单例

---
+++ UnityTool/Assets/Script/Util/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Singleton<T> where T : class, new()
+{
+	private static T mInstance = default(T);
+	
+	public static T Instance {
+		get {
+			if (Singleton<T>.mInstance == null) {
+				Singleton<T>.mInstance = Activator.CreateInstance<T> ();
+			}
+			return Singleton<T>.mInstance;
+		}
+	}
+}
+
