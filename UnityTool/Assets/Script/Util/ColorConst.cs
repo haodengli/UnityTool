9a8970f62f96e0f57a025f30197128b9f386c0fe
haodengli <1251280100@qq.com>
2015-09-28 11:31:23 +0800

    color const

---
+++ UnityTool/Assets/Script/Util/ColorConst.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+public class ColorConst
+{
+	public const string RED = "[FF0000]";
+	public const string GREEN = "[01FF00]";
+	public const string BLUE = "[0100FF]";
+	public const string WHITE = "[FFFFFF]";
+	public const string BLACK = "[010000]";
+	public const string GRAY = "[888888]";
+	public const string BROWN = "[3F2A11]";
+	public const string YELLOW = "[FFFF00]";
+	public const string MIDDLE_YELLOW = "[F3BF6A]";
+	public const string EASY_YELLOW = "[F8E1B5]";
+	public const string BROWN_YELLOW = "[FFD800]";
+}
